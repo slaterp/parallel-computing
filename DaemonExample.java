56b8d472a2074e7a8b0e4fead72fa74984a3e43f
Patrick <patrickslater97@gmail.com>
2021-05-25 15:00:28 +1000

    Added simple example of Daemon threads

---
+++ DaemonExample.java
@@ -0,0 +1,50 @@
+
+/*
+ * This program is an illustrative example of the difference between a Daemon thread and Main (worker) thread.
+ * In short, Daemon threads will continue execution until the program has finished running. In this program, completion
+ * brought on by the end of the NormalWorker thread (which simply waits for 3 seconds before terminating).
+ */
+
+class DaemonWorker implements Runnable {
+
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Daemon is running");
+		}
+	}
+	
+}
+
+class NormalWorker implements Runnable {
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Normal worker had completed execution (thread done)");
+	}
+	
+}
+
+public class DaemonExample {
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread(new DaemonWorker());
+		Thread t2 = new Thread(new NormalWorker());
+		
+		t1.setDaemon(true);
+		
+		t1.start();
+		t2.start();
+	}
+
+}
