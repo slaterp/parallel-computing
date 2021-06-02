2631b2c6666973204ba5deef19d13b36a7486854
Patrick <patrickslater97@gmail.com>
2021-06-02 10:47:19 +1000

    Callable interface demo

---
+++ s6Executors/CallableDemo.java
@@ -0,0 +1,54 @@
+package s6Executors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+class Processor implements Callable<String> {
+	
+	private int id;
+	
+	public Processor(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(1000);
+		return "Id: " + id;
+	}
+	
+}
+
+public class CallableDemo {
+
+	public static void main(String[] args) {
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+		List<Future<String>> list = new ArrayList<>();
+		
+		for (int i = 0; i < 5; i++) {
+			Future<String> future = executorService.submit(new Processor(i+1));
+			list.add(future);
+		}
+		
+		for (Future<String> future : list) {
+			try {
+				System.out.println(future.get());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		executorService.shutdown();
+		
+	}
+
+}
